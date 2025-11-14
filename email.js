7188d8f7589256c5629fd6384cb69c0ff533c87f
Selman <selmanebrahim567@gmail.com>
2025-11-14 17:15:33 +0300

    nat

--- email.js
+++ email.js
@@ -1,13 +1,10 @@
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
- host: 'smtp.example.com',
- port: 587,
- secure: false, 
- auth: {
-   user: 'natnaelmessay71@gmail.com',
-   pass: 'qici bqim mrzc hppr' 
- }
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'natnaelmessay71@gmail.com',
+    pass: 'qici bqim mrzc hppr', 
+  },
 });
-
 module.exports = transporter;
